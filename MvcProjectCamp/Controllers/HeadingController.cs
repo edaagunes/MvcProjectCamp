071fe6a96f35329d4133e287bdf8beb8d4a46ac6
edaagunes <edagunes00@hotmail.com>
2024-11-02 20:42:50 +0300

    Added HeadingService,Manager,EfHeadingDal and Heading list iterations

---
+++ MvcProjectCamp/Controllers/HeadingController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager manager = new HeadingManager(new EfHeadingDal());
+        public ActionResult Index()
+        {
+            var headingValues = manager.GetList();
+            return View(headingValues);
+        }
+    }
+}
