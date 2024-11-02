175393a99e20097787a2a509d8c063c40c065246
edaagunes <edagunes00@hotmail.com>
2024-11-02 20:13:05 +0300

    Added WriterManager,Service,Validator and AddWriter

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class WriterValidator:AbstractValidator<Writer>
+	{
+		public WriterValidator()
+		{
+			RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adı Boş Geçilemez!");
+			RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadı Boş Geçilemez!");
+			RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda Boş Bırakılamaz!");
+			RuleFor(x => x.WriterAbout).Must(x => x.Contains("a") || x.Contains("A")).WithMessage("Hakkımda Metninde A-a Harfi Geçmelidir.");
+			RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda Boş Bırakılamaz!");
+			RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen En Az 2 Karakter Girişi Yapınız!");
+			RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen En Fazla 50 Karakter Girişi Yapınız!");
+		}
+	}
+}
---
+++ MvcProjectCamp/Controllers/WriterController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+	public class WriterController : Controller
+	{
+		WriterManager manager = new WriterManager(new EfWriterDal());
+		public ActionResult Index()
+		{
+			var writerValues = manager.GetList();
+			return View(writerValues);
+		}
+
+		[HttpGet]
+		public ActionResult AddWriter()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult AddWriter(Writer writer)
+		{
+			WriterValidator writerValidator = new WriterValidator();
+			ValidationResult results = writerValidator.Validate(writer);
+			if (results.IsValid)
+			{
+				manager.WriterAdd(writer);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
+		}
+	}
+}
