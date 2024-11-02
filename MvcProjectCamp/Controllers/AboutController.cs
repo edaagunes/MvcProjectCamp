b5f63e0fb9bee07c7dd659768e7f381d7f3c0bfb
edaagunes <edagunes00@hotmail.com>
2024-11-03 01:04:37 +0300

    Added Modal in AboutController

---
+++ MvcProjectCamp/Controllers/AboutController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class AboutController : Controller
+    {
+       AboutManager aboutManager=new AboutManager(new EfAboutDal());
+        public ActionResult Index()
+        {
+            var aboutValues=aboutManager.GetList();
+            return View(aboutValues);
+        }
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+		public ActionResult AddAbout(About about)
+		{
+            aboutManager.AboutAdd(about);
+			return RedirectToAction("Index");
+		}
+	}
+}
