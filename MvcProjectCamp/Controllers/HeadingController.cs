a0d5f2a124eae3eeb5fd4caa1d49164da0aacd96
edaagunes <edagunes00@hotmail.com>
2024-11-02 21:11:33 +0300

    Added AddHeading and DropDownList

--- MvcProjectCamp/Controllers/HeadingController.cs
+++ MvcProjectCamp/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,42 @@ using System.Web.Mvc;
 
 namespace MvcProjectCamp.Controllers
 {
-    public class HeadingController : Controller
-    {
-        HeadingManager manager = new HeadingManager(new EfHeadingDal());
-        public ActionResult Index()
-        {
-            var headingValues = manager.GetList();
-            return View(headingValues);
-        }
-    }
+	public class HeadingController : Controller
+	{
+		HeadingManager manager = new HeadingManager(new EfHeadingDal());
+		CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+		WriterManager writerManager = new WriterManager(new EfWriterDal());
+		public ActionResult Index()
+		{
+			var headingValues = manager.GetList();
+			return View(headingValues);
+		}
+		[HttpGet]
+		public ActionResult AddHeading()
+		{
+			List<SelectListItem> valueCategory = (from x in categoryManager.GetList()
+												  select new SelectListItem
+												  {
+													  Text = x.CategoryName,
+													  Value = x.CategoryId.ToString()
+												  }).ToList();
+
+			List<SelectListItem> valueWriter = (from x in writerManager.GetList()
+												  select new SelectListItem
+												  {
+													  Text = x.WriterName+" "+x.WriterSurname,
+													  Value = x.WriterId.ToString()
+												  }).ToList();
+			ViewBag.valueCategory=valueCategory;
+			ViewBag.valueWriter=valueWriter;
+			return View();
+		}
+		[HttpPost]
+		public ActionResult AddHeading(Heading heading)
+		{
+			heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+			manager.HeadingAdd(heading);
+			return RedirectToAction("Index");
+		}
+	}
 }
