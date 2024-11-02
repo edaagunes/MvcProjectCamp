1ddccc20ee6a8df5ebee59b8d6d74677d2f6c34c
edaagunes <edagunes00@hotmail.com>
2024-11-02 18:55:43 +0300

    Added Category iterations and Statistics

--- MvcProjectCamp/Controllers/AdminCategoryController.cs
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +11,58 @@ using System.Web.Mvc;
 
 namespace MvcProjectCamp.Controllers
 {
-    public class AdminCategoryController : Controller
-    {
-        // GET: AdminCategory
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class AdminCategoryController : Controller
+	{
+		CategoryManager manager = new CategoryManager(new EfCategoryDal());
+		public ActionResult Index()
+		{
+			var categoryValues = manager.GetList();
+			return View(categoryValues);
+		}
+
+		[HttpGet]
+		public ActionResult AddCategory()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult AddCategory(Category category)
+		{
+			CategoryValidator categoryValidator = new CategoryValidator();
+			ValidationResult results = categoryValidator.Validate(category);
+			if (results.IsValid)
+			{
+				manager.CategoryAdd(category);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
+		}
+
+		public ActionResult DeleteCategory(int id)
+		{
+			var categoryValue=manager.GetById(id);
+			manager.CategoryDelete(categoryValue);
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public ActionResult EditCategory(int id)
+		{
+			var categoryValue=manager.GetById(id);
+			return View(categoryValue);
+		}
+
+		[HttpPost]
+		public ActionResult EditCategory(Category category)
+		{
+			manager.CategoryUpdate(category);
+			return RedirectToAction("Index");
+		}
+	}
 }
---
+++ MvcProjectCamp/Controllers/AdminStatisticController.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+	public class AdminStatisticController : Controller
+	{
+		Context context = new Context();
+		public ActionResult Index()
+		{
+			ViewBag.totalCategory = context.Categories.Count();
+			ViewBag.heading = context.Headings.Where(x => x.CategoryId == 9).Count();
+			ViewBag.author = context.Writers.Where(x => x.WriterName.Contains("a") || x.WriterName.Contains("A") ||
+				x.WriterSurname.Contains("a") || x.WriterSurname.Contains("A")).Count();
+			ViewBag.maxCategory = context.Headings.GroupBy(h => h.Category.CategoryName).OrderByDescending(g => g.Count()).Select(g => g.Key).FirstOrDefault();
+			ViewBag.status = context.Categories.Where(x => x.CategoryStatus == true).Count() - context.Categories.Where(x => x.CategoryStatus == false).Count();
+			return View();
+		}
+	}
+}
