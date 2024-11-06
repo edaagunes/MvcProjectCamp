b282209ade4a65f6ade4d3501772622514761c0c
edaagunes <edagunes00@hotmail.com>
2024-11-06 15:15:09 +0300

    Added Gallery

---
+++ MvcProjectCamp/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ImageFileManager manager=new ImageFileManager(new EfImageFileDal());
+        public ActionResult Index()
+        {
+            var files = manager.GetList();
+            return View(files);
+        }
+    }
+}
